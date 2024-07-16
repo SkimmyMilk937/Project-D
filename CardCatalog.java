5a495b3e4072ba05e02e6d5a9a9bd9b07b1d8f09
Ace8987 <adclun3@gmail.com>
2024-07-16 11:53:27 -0700

    Update CardCatalog.java
    Added Bibimbap crew with placeholder values

--- CardCatalog.java
+++ CardCatalog.java
@@ -1,5 +1,13 @@
 public enum CardCatalog {
-    Arian("Bibimbap", "fighter", 1);
+    Arian("Bibimbap", "fighter", 1),
+    Avery("Bibimbap", "fighter", 1),
+	Brendan("Bibimbap", "fighter", 1),
+	Adam("Bibimbap", "fighter", 1),
+	Idin("Bibimbap", "fighter", 1),
+	Nic("Bibimbap", "fighter", 1),
+	Sebastian("Bibimbap", "fighter", 1),
+	Soushyant("Bibimbap", "fighter", 1),
+	Hanseo("Bibimbap", "fighter", 1);
 
     public final String set;
     public final String type;
