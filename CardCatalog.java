967316ee5407102cd3d60da8eebec650c4dcade0
Ace8987 <adclun3@gmail.com>
2024-07-16 22:36:37 -0700

    Update CardCatalog.java

--- CardCatalog.java
+++ CardCatalog.java
@@ -1,21 +1,26 @@
+
+import java.io.File;
+
 public enum CardCatalog {
-    Arian("Bibimbap", "fighter", 1),
-    Avery("Bibimbap", "fighter", 1),
-	Brendan("Bibimbap", "fighter", 1),
-	Adam("Bibimbap", "fighter", 1),
-	Idin("Bibimbap", "fighter", 1),
-	Nic("Bibimbap", "fighter", 1),
-	Sebastian("Bibimbap", "fighter", 1),
-	Soushyant("Bibimbap", "fighter", 1),
-	Hanseo("Bibimbap", "fighter", 1);
+    Arian("Bibimbap", "fighter", 1, new File("Images/Arian.png")),
+	Avery("Bibimbap", "fighter", 1, new File("Images/Avery.png")),
+	Brendan("Bibimbap", "fighter", 1, new File("Images/Brendan.png")),
+	Adam("Bibimbap", "fighter", 1, new File("Images/Adam.png")),
+	Idin("Bibimbap", "fighter", 1, new File("Images/Idin.png")),
+	Nic("Bibimbap", "fighter", 1, new File("Images/Nic.png")),
+	Sebastian("Bibimbap", "fighter", 1, new File("Images/Sebastian.png")),
+	Soushyant("Bibimbap", "fighter", 1, new File("Images/Soushyant.png")),
+	Hanseo("Bibimbap", "fighter", 1, new File("Images/Hanseo.png"));
 
     public final String set;
     public final String type;
     public final int cost;
+    public final File image;
 
-    CardCatalog(String set, String type, int cost){
+    CardCatalog(String set, String type, int cost, File image){
         this.set = set;
         this.type = type;
         this.cost = cost;
+        this.image = image;
     }
 }
