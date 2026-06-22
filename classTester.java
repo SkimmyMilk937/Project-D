1e44606d922be92e757340a3eee221ab60c47c7b
Nicolas Attefat <110652867+SkimmyMilk937@users.noreply.github.com>
2026-06-21 18:12:39 -0700

    Rename child element from 'penisislarge' to 'll'

--- classTester.java
+++ classTester.java
@@ -3,13 +3,13 @@ import org.w3c.dom.Element;
 public class classTester {
     public static void main(String args[]){
         XMLWriter writer = new XMLWriter("deck4.xml", true);
-        Element x = writer.createNewChildElement(writer.getRootElement(), "penisislarge");
+        Element x = writer.createNewChildElement(writer.getRootElement(), "ll");
         writer.createTextElement(x, "hi", "hi");
 
         //Element x = writer.createNewRootElement("gayt");
         //writer.createTextElement(writer.getRootElement(), "gaty", "large");
-        //Element element = writer.createNewChildElement(writer.getRootElement(), "mypenis");
-        //writer.createTextElement(writer.getRootElement(), "Program4", "mypenis is huge");
+        //Element element = writer.createNewChildElement(writer.getRootElement(), "ll");
+        //writer.createTextElement(writer.getRootElement(), "Program4", "lll");
         //writer.createTextElement(root, "what is it?", "im the goat");
         //XMLWriter newFile = new XMLWriter("deckballs.xml");
         //newFile.createNewRootElement("Cards");
